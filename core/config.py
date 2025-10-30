d69f99d1b99e18a2effb28778c3d1158dff182e6
nasser <203167218+nasserx@users.noreply.github.com>
2025-10-31 00:43:57 +0300

    refactor: update file paths to use BASE_DIR for better structure

--- core/config.py
+++ core/config.py
@@ -1,16 +1,9 @@
 from pathlib import Path
 
-# Data and directories 
-DATA_DIR = Path("data")
-DATA_DIR.mkdir(exist_ok=True)  # Create the folder if it does not exist
 
-ASSETS_DIR = Path("assets")
-ICONS_DIR = ASSETS_DIR / "icons"
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Data files 
-TRANSACTIONS_FILE = DATA_DIR / "transactions.json"
-PASSCODE_FILE = DATA_DIR / "passcode.json"
-
-# Application settings 
+TRANSACTIONS_FILE = BASE_DIR / "core" / "data" / "transactions.json"
+PASSCODE_FILE = BASE_DIR / "core" / "data" / "passcode.json"
+APP_ICON = BASE_DIR / "assets" / "icons" / "app.ico"
 APP_TITLE = "Personal Account Statement"
-APP_ICON = ICONS_DIR / "app.ico"
